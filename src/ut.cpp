4f44ebe673afdf287c5b9c9f33c75bb818b2dd80
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2022-05-27 14:09:34 +0200

    Add tests for empty Db
    Refactor tests

--- src/ut.cpp
+++ src/ut.cpp
@@ -2,7 +2,12 @@
 #include "database.hpp"
 #include "student.hpp"
 
-TEST(CheckStructure, CanAddStudentToDatabase)
+struct DatabaseTest : ::testing::Test
+{
+    Database db;
+};
+
+TEST_F(DatabaseTest, CanAddStudentToDatabase)
 {
     Student adam{
         "Adam",
@@ -13,7 +18,31 @@ TEST(CheckStructure, CanAddStudentToDatabase)
         Gender::Male
     };
 
-    Database db;
     EXPECT_TRUE(db.add(adam));
     EXPECT_FALSE(db.add(adam));
 }
+
+TEST_F(DatabaseTest, DisplayEmtyDatabase)
+{
+    auto content = db.show();
+    auto expected = "";
+    EXPECT_EQ(content,expected);
+}
+
+// TEST(DisplayDatabase, DisplayNonEmptyDatabase)
+// {
+//     Database db;
+
+//      Student adam{
+//         "Adam",
+//         "Nowak",
+//         "ul.Bogatynska 12 00-201 Radom",
+//         123456,
+//         "12345678912",
+//         Gender::Male
+//     };
+//     EXPECT_TRUE(db.add(adam));
+
+//     db.display();
+// }
+
