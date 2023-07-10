83eb924e66f16296cce9a2714a9fee347adbd720
Jaroslaw <roszyk.jarek@gmail.com>
2023-07-10 21:49:48 +0200

    Refactor: introdouce mocks

--- uts/FileHandler_tests.cpp
+++ uts/FileHandler_tests.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <fstream>
 #include "Database.hpp"
 #include "FileHandler.hpp"
 #include "Student.hpp"
+
+using namespace testing;
 
 namespace
 {
@@ -10,13 +13,23 @@ namespace
     const int sizeOfDb = 2;
 } // namespace
 
+class MockFileHandler : public FileHandler
+{
+public:
+    MockFileHandler(const std::string& filename, Mode mode) : FileHandler(filename, mode) {}
+
+    MOCK_METHOD(void, writeToCsvFile, (const Database& db), ());
+    MOCK_METHOD(void, readDbFromFile, (Database& db), ());
+};
+
+
 class FileHandlerTest : public ::testing::Test
 {
 protected:
     void SetUp() override
     {
         testFileName = FILENAME;
-        fileHandler = std::make_unique<FileHandler>(testFileName, Mode::Write);
+        mockFileHandler = std::make_unique<MockFileHandler>(testFileName, Mode::Write);
         db = std::make_unique<Database>();
 
         db->add(Student("John", "Doe", "123 Main St", 12345, "1234567890", Gender::Male));
@@ -28,14 +41,31 @@ protected:
         std::remove(testFileName.c_str());
     }
 
-    std::unique_ptr<FileHandler> fileHandler;
+    std::unique_ptr<MockFileHandler> mockFileHandler;
     std::unique_ptr<Database> db;
     std::string testFileName;
 };
 
 TEST_F(FileHandlerTest, WriteToCsvFile)
 {
-    fileHandler->writeToCsvFile(*db);
+    EXPECT_CALL(*mockFileHandler, writeToCsvFile(testing::_))
+        .WillOnce(testing::Invoke([&](const Database& db)
+            {
+                std::ofstream fileStream(testFileName);
+                fileStream << "Name, Surname, Address, Pesel, Gender\n";
+                for (const auto& student : db.getStudents())
+                {
+                    fileStream << student.getName() << ", "
+                        << student.getSurname() << ", "
+                        << student.getAddress() << ", "
+                        << student.getPesel() << ", "
+                        << GenderToString(student.getGender()) << "\n";
+                }
+                fileStream.flush();
+                fileStream.close();
+            }));
+
+    mockFileHandler->writeToCsvFile(*db);
 
     std::ifstream fileStream(testFileName);
     EXPECT_TRUE(fileStream.is_open());
@@ -54,14 +84,42 @@ TEST_F(FileHandlerTest, WriteToCsvFile)
 
 TEST_F(FileHandlerTest, WriteToCsvFile_InvalidFileMode)
 {
-    fileHandler = std::make_unique<FileHandler>(testFileName, Mode::Read);
-    EXPECT_THROW(fileHandler->writeToCsvFile(*db), std::runtime_error);
+    EXPECT_CALL(*mockFileHandler, writeToCsvFile(testing::_))
+        .WillOnce(testing::Invoke([&](const Database& /*db*/)
+            {
+                throw std::runtime_error("Invalid file mode");
+            }));
+
+    EXPECT_THROW(mockFileHandler->writeToCsvFile(*db), std::runtime_error);
 }
 
 TEST_F(FileHandlerTest, ReadDbFromFile)
 {
-    fileHandler = std::make_unique<FileHandler>(testFileName, Mode::Read);
-    fileHandler->readDbFromFile(*db);
+    EXPECT_CALL(*mockFileHandler, readDbFromFile(testing::_))
+        .WillOnce(testing::Invoke([&](Database& db)
+            {
+                std::ifstream fileStream(testFileName);
+                std::string line{};
+                std::getline(fileStream, line); // skip HEADER Name, Surname, Address, Pesel, Gender 
+
+                while (std::getline(fileStream, line))
+                {
+                    std::istringstream iss(line);
+                    std::string name, surname, address, pesel, genderStr;
+                    std::getline(iss, name, ',');
+                    std::getline(iss, surname, ',');
+                    std::getline(iss, address, ',');
+                    std::getline(iss, pesel, ',');
+                    std::getline(iss, genderStr, ',');
+
+                    Gender gender = StringToGender(genderStr);
+                    db.add(Student(name, surname, address, 0, pesel, gender));
+                }
+
+                fileStream.close();
+            }));
+
+    mockFileHandler->readDbFromFile(*db);
 
     ASSERT_EQ(db->getSize(), sizeOfDb);
     ASSERT_TRUE(db->searchByIndex(12345));
@@ -72,6 +130,11 @@ TEST_F(FileHandlerTest, ReadDbFromFile)
 
 TEST_F(FileHandlerTest, ReadDbFromFile_InvalidFileMode)
 {
-    fileHandler = std::make_unique<FileHandler>(testFileName, Mode::Write);
-    EXPECT_THROW(fileHandler->readDbFromFile(*db), std::runtime_error);
+    EXPECT_CALL(*mockFileHandler, readDbFromFile(testing::_))
+        .WillOnce(testing::Invoke([&](Database& /*db*/)
+            {
+                throw std::runtime_error("Invalid file mode");
+            }));
+
+    EXPECT_THROW(mockFileHandler->readDbFromFile(*db), std::runtime_error);
 }
